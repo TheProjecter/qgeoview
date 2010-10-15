9a75297e6282c677ce9225250aca482ab7556c82
darwinsurvivor <darwinsurvivor@9185ca4a-700f-4767-d84f-434ff1afccf3>
2010-10-15 13:39:29 +0000

    Added sql import code, getting a strange "Parameter Count" error though :( Added a reference to "cache" in the Logs table of the database (oversight)

--- database.h
+++ database.h
@@ -4,12 +4,86 @@
 #include <QString>
 #include <QSqlDatabase>
 #include <QObject>
+#include <QFile>
+
+typedef struct point_ {
+    int id;
+    QString time;
+    float elevation;
+    float magneticVariation;
+    float geoIDHeight;
+    QString symbol;
+    QString fix;
+    int satelites;
+    float horizontalDOP;
+    float verticalDOP;
+    float positionDOP;
+    float ageOfDGPSData;
+    int DGPSID;
+    float latitude;
+    float longitude;
+} Point;
+
+typedef struct log_ {
+    int id;
+    long log_id;
+    QString log_guid;
+    QString date;
+    QString type;
+    long finder_id;
+    QString finder_guid;
+    QString finder_name;
+    QString text;
+    bool text_encoded;
+    float latitude;
+    float longitude;
+    int cache;
+} Log;
+
+typedef struct cache_ {
+    int id;
+    QString name;
+    QString placed_by;
+    long owner_id;
+    QString owner_guid;
+    QString owner_name;
+    QString type;
+    QString container;
+    float difficulty;
+    float terrain;
+    QString country;
+    QString state;
+    QString short_description;
+    bool short_description_encoded;
+    QString long_description;
+    bool long_description_encoded;
+    QString encoded_hints;
+    int waypoint;
+} Cache;
+
+typedef struct waypoint_ {
+    int id;
+    int description;
+    int point;
+} Waypoint;
+
+typedef struct description_ {
+    int id;
+    QString name;
+    QString link_url;
+    QString link_name;
+    QString comments;
+    QString source;
+    QString type;
+} Description;
 
 class Database : public QObject
 {
 Q_OBJECT
 public:
     explicit Database(QString location, QObject *parent = 0);
+    bool import_gpx(QFile *file);
+    QFile export_gpx();
     ~Database();
 private:
     QSqlDatabase _db;
