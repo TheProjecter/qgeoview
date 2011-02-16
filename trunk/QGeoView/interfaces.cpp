5579729b742da2a41622b7d73fb117988c43f153
darwinsurvivor <darwinsurvivor@9185ca4a-700f-4767-d84f-434ff1afccf3>
2011-02-16 10:19:45 +0000

    removed useless interfaces.cpp file removed read/write/mode plugin support, getting dummyplugin working first

--- trunk/QGeoView/interfaces.cpp
+++
@@ -1,7 +0,0 @@
-#include "interfaces.h"
-
-void ReadPlugin::setup(Database *db)
-{
-    _db = db;
-}
-
--- trunk/QGeoView/interfaces.h
+++ trunk/QGeoView/interfaces.h
@@ -2,71 +2,19 @@
 #define INTERFACES_H
 
 #include <QtPlugin>
-#include <QSqlDatabase>
-#include <QString>
-#include <QFile>
 
-#include "db/database.h"
-
-#define COMPATIBLE_NO       0
-#define COMPATIBLE_YES      1
-#define COMPATIBLE_MAYBE    2
-
-class ReadPlugin
+class DummyPlugin
 {
 public:
-    virtual void setup(Database *db) = 0;
-    virtual QString name() = 0;
-    virtual void read(QFile *file) = 0;
-private:
-    Database *_db;
+    virtual void print() = 0;
 };
 
-class WritePlugin
+class DummyPluginFactory
 {
 public:
-    WritePlugin(Database *db);
-    QString name() { return "Abstract"; }
-    void write(QFile &file, int collection) {}
-private:
-    Database *_db;
+    virtual DummyPlugin *get_plugin() = 0;
 };
 
-class ModePlugin
-{
-public:
-    virtual QString name() = 0;
-public slots:
-    void start() {};
-    void stop() {};
-    void activate() {};
-    void deactivate() {};
-    void selected() {};
-    void deselected() {};
-};
-
-class ReadPluginFactory
-{
-public:
-    virtual ~ReadPluginFactory() = 0;
-    virtual ReadPlugin *get_plugin() = 0;
-    virtual ReadPlugin *get_plugin(Database *db) = 0;
-};
-
-class WritePluginFactory
-{
-public:
-    virtual WritePlugin *get_plugin(Database *db) = 0;
-};
-
-class ModePluginFactory
-{
-public:
-    virtual ModePlugin *get_plugin() = 0;
-};
-
-Q_DECLARE_INTERFACE(ReadPluginFactory, "org.homelinux.darwinsurvivor.QGeoView.ReadPluginFactory");
-Q_DECLARE_INTERFACE(WritePluginFactory, "org.homelinux.darwinsurvivor.QGeoView.WritePluginFactory");
-Q_DECLARE_INTERFACE(ModePluginFactory, "org.homelinux.darwinsurvivor.QGeoView.ModePluginFactory");
+Q_DECLARE_INTERFACE(DummyPluginFactory, "org.homelinux.darwinsurvivor.QGeoView.DummyPluginFactory");
 
 #endif
