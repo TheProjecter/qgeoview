3ea2efcb7bae77e33618f3c31715500af30dde08
darwinsurvivor <darwinsurvivor@9185ca4a-700f-4767-d84f-434ff1afccf3>
2011-02-28 01:00:44 +0000

    Moved "Accuracy" into "Point" to match database. Got insert and update started, shouldn't take much longer to get it working for Point at least.

--- trunk/QGeoView/db/point.cpp
+++ trunk/QGeoView/db/point.cpp
@@ -4,3 +4,46 @@ Point::Point(QObject *parent) :
     DatabaseObject(parent)
 {
 }
+
+Point::~Point()
+{
+    delete _accuracy;
+}
+
+QStringList Point::fields()
+{
+    QStringList list;
+    list.append("time");
+    list.append("elevation");
+    list.append("magneticVariation");
+    list.append("geoIDHeight");
+    list.append("symbol");
+    list.append("fix");
+    list.append("satelites");
+    list.append("horizontalDOP");
+    list.append("verticalDOP");
+    list.append("positionDOP");
+    list.append("ageofDGPSData");
+    list.append("DGPSID");
+    list.append("latitude");
+    list.append("longitude");
+    return list;
+}
+
+void Point::addBindValues(QSqlQuery query)
+{
+    query.addBindValue(isSet(NULLMASK_TIME) ? _time : QVariant(QVariant::String));
+    query.addBindValue(isSet(NULLMASK_ELEVATION) ? _elevation : QVariant(QVariant::Double));
+    query.addBindValue(isSet(NULLMASK_MAGNETICVARIATION) ? _magneticVariation : QVariant(QVariant::Double));
+    query.addBindValue(isSet(NULLMASK_GEOIDHEIGHT) ? _geoIDHeight : QVariant(QVariant::Double));
+    query.addBindValue(isSet(NULLMASK_SYMBOL) ? _symbol : QVariant(QVariant::String));
+    query.addBindValue(isSet(NULLMASK_FIX) ? _fix : QVariant(QVariant::String));
+    query.addBindValue(isSet(NULLMASK_SATELITES) ? _satelites : QVariant(QVariant::Int));
+    query.addBindValue(isSet(NULLMASK_HORIZONTALDOP) ? _horizontalDOP : QVariant(QVariant::Double));
+    query.addBindValue(isSet(NULLMASK_VERTICALDOP) ? _verticalDOP : QVariant(QVariant::Double));
+    query.addBindValue(isSet(NULLMASK_POSITIONDOP) ? _positionDOP : QVariant(QVariant::Double));
+    query.addBindValue(isSet(NULLMASK_AGEOFDGPSDATA) ? _ageOfDGPSData : QVariant(QVariant::Double));
+    query.addBindValue(isSet(NULLMASK_DGPSID) ? _DGPSID : QVariant(QVariant::Int));
+    query.addBindValue(isSet(NULLMASK_LATITUDE) ? _latitude : QVariant(QVariant::Double));
+    query.addBindValue(isSet(NULLMASK_LONGITUDE) ? _longitude : QVariant(QVariant::Double));
+}
--- trunk/QGeoView/db/point.h
+++ trunk/QGeoView/db/point.h
@@ -4,16 +4,38 @@
 #include <QObject>
 #include "databaseobject.h"
 #include "accuracy.h"
+#include "db/exceptions.h"
+
+// NULLMASK_ID is set in db/databaseobject.h ans is 0x1 DO NOT USE 0x1 HERE!!!
+#define NULLMASK_TIME               0x2
+#define NULLMASK_ELEVATION          0x4
+#define NULLMASK_MAGNETICVARIATION  0x8
+#define NULLMASK_GEOIDHEIGHT        0x10
+#define NULLMASK_SYMBOL             0x20
+#define NULLMASK_LATITUDE           0x40
+#define NULLMASK_LONGITUDE          0x80
+#define NULLMASK_ACCURACY           0x100
+#define NULLMASK_FIX                0x200
+#define NULLMASK_SATELITES          0x400
+#define NULLMASK_HORIZONTALDOP      0x800
+#define NULLMASK_VERTICALDOP        0x1000
+#define NULLMASK_POSITIONDOP         0x2000
+#define NULLMASK_AGEOFDGPSDATA      0x4000
+#define NULLMASK_DGPSID             0x8000
 
 class Point : public DatabaseObject
 {
     Q_OBJECT
 public:
-    explicit Point(QObject *parent = 0);
+    Point(QObject *parent = 0);
+    ~Point();
+    void addBindValues(QSqlQuery query);
 
 signals:
 
 public slots:
+protected:
+    QStringList fields();
 private:
     QString _time;
     float _elevation;
@@ -22,6 +44,14 @@ private:
     QString _symbol;
     float _latitude;
     float _longitude;
+    QString _fix;
+    int _satelites;
+    float _horizontalDOP;
+    float _verticalDOP;
+    float _positionDOP;
+    float _ageOfDGPSData;
+    int _DGPSID;
+
     Accuracy *_accuracy;
 
 };
